3ce5be3b0b5a7ffa1383309deb79d6d9eac42742
Ujjwal <ujjwalgupta303@gmail.com>
2021-12-18 17:25:30 +0530

    Added gin for routing

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hi Ujjwal")
+	router := gin.Default()
+	router.GET("/",func(c *gin.Context) {
+		c.JSON(200,gin.H{
+			"message": "Wecome to Tiny URL Service",
+		})
+	})
+
+	err := router.Run(":8080")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
 }
